56f6caa2b6f1ff1041bd1163a575fb4dd7eecaf5
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-05-08 01:46:02 +0200

    Fix indentation on Main

--- Source/Main.hx
+++ Source/Main.hx
@@ -8,18 +8,18 @@ import openfl.events.Event;
 import yaml.Yaml;
 
 class Main extends Sprite {
-    var rect:Rectangle = new Rectangle();
+    private var testRectangle:Rectangle = new Rectangle();
     
-	public function new () {
-		super ();
-        addChild(rect);
+    public function new () {
+        super ();
+        addChild(testRectangle);
         Lib.current.stage.displayState = StageDisplayState.FULL_SCREEN_INTERACTIVE;
         addEventListener(Event.ENTER_FRAME, gameLoop);
-	}
+    }
     
     private function gameLoop(e:Event):Void 
     {
         RectTransform.windowSize.setWidthHeight(stage.stageWidth, stage.stageHeight);
-        rect.doAction();
+        testRectangle.doAction();
     }
 }
